489bfdc3537e6b0de9ed9cadbcabc600669ab2cf
jiangyishan <Yjiang@lcs.on.ca>
2026-06-01 11:27:49 -0400

    Initial Commit

---
+++ ICS3UCulminating /ICS3UCulminating App.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3UCulminating App.swift
+//  ICS3UCulminating 
+//
+//  Created by Student on 1/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct ICS3UCulminating_App: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
